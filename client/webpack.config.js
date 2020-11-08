b62e56f68873effbce09ce68ed09143f84c28ac6
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2020-11-08 11:03:58 +0100

    Added favicon and page title

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,7 @@ module.exports = function (env) {
         },
         plugins: [
             new HtmlWebpackPlugin({
+                favicon: './src/favicon.png',
                 template: './src/index.html'
             })
         ]
